40f5663e78412a699793d99966e0f595c5d03270
Ettore Mosca <ettore.mosca@itb.cnr.it>
2020-09-17 17:29:04 +0200

    scMuffin

--- R/scMuffin.R
+++ R/scMuffin.R
@@ -1,14 +1,25 @@
 #' scMuffin: main function that runs the whole pipeline
-#' @param genes_by_cells genes-by-cells input matrix
+#' @param genes_by_cells seurat_object_data, genes-by-cells input matrix
+#' @param custom_signatures list of gene sets
+#' @import parallel
 
-
-scMuffin <- function(genes_by_cells){
-
-  #signature expression @Ettore
+scMuffin <- function(genes_by_cells, custom_signatures=NULL){
 
 
   #Seurat clustering @Noemi
 
+  #seurat_clusters <-
+
+  #signature expression @Ettore
+  if(!is.null(custom_signatures)){
+    signatures <- c(signatures, custom_signatures)
+  }
+  print(signatures)
+
+  data_bins <- sc_data_bin(genes_by_cells, nbins = 25, use.log = TRUE)
+
+  #parallel
+  res <- mclapply(signatures, function(i_marker_set) gene_set_score_in_clusters(i_marker_set, seurat_object_data = genes_by_cells, seurat_object_ident = seurat_clusters, perc=0.25, bins = data_bins, k=100, alt = "two.sided", test="t", nmark_min = 5, ncells_min=10), mc.cores = min(length(all_markers_sets), 2))
 
   #Signalling entropy rate (SR) @Noemi
 
