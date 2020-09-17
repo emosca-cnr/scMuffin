ce9fb31e20749634707f9ef57a15a142a5375225
Ettore Mosca <ettore.mosca@itb.cnr.it>
2020-09-17 16:59:46 +0200

    scMuffin

---
+++ R/scMuffin.R
@@ -0,0 +1,39 @@
+#' scMuffin: main function that runs the whole pipeline
+#' @param genes_by_cells genes-by-cells input matrix
+
+
+scMuffin <- function(genes_by_cells){
+
+  #signature expression @Ettore
+
+
+  #Seurat clustering @Noemi
+
+
+  #Signalling entropy rate (SR) @Noemi
+
+
+  #Potency states (LandScent): labels @Noemi
+
+
+  #Diffusion pseudotime (DPT) (Destiny): @Noemi
+
+  #Paride score @Noemi
+
+  #Cell cycle state TO DO
+
+
+  #CNV @Valentina
+
+
+  #merge everithing
+
+
+  #re-clustering
+
+
+  #Plots and Tables as outputs
+
+
+
+}
