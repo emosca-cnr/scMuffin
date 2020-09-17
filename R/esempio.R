81b8ebd529a2b78fa3107e9c53a6591f42442ee1
Ettore Mosca <emosca@dues-iMac.local>
2020-09-17 16:10:23 +0200

    new file:   .gitignore 	new file:   R/esempio.R

---
+++ R/esempio.R
@@ -0,0 +1,13 @@
+#' Titolo della Funzione di esempio
+#' @param x descrizione parametro x
+#' @param y descrizione parametro y
+#' @import libreria_necessaria
+
+
+esempio <- function(x, y){
+
+
+  #esempio chiamata funzioni di librerie importate
+  libreria_necessaria::funzione_necessaria
+
+}
