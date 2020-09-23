1f40535be677996a113687ac74405d79dc6c0f16
Valentina <valentina.nale@itb.cnr.it>
2020-09-23 12:03:24 +0200

    Valentina

---
+++ R/preprocess_for_heatmap.R
@@ -0,0 +1,19 @@
+#' preprocess_for_heatmap
+#' @param result_cnv 
+#' @description Bind chromosomes together as a matrix. 
+#' @details Every chromosome is first transposed. 
+#' @usage preprocess_for_heatmap(result_cnv)
+#' @return A matrix to be used by the function heatmap_CNV.
+#' @author Valentina Nale
+
+heatmap_cnv <- function(result_cnv) {
+	for (i in 1:length(result_cnv)) {
+		temp <- paste0("cnv_cnr", i)
+		print(temp)
+		temp <- result_cnv[[i]]
+		temp <- t(temp)	
+		chr_merged <- matrix(nrow= nrow(temp))
+		chr_merged <- cbind(chr_merged, temp)
+		chr_merged <<- chr_merged
+	}
+}
