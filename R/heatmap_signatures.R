fe2e914b75907d3b0914d35aed64578d04a4346e
emosca-cnr <ettore.mosca@itb.cnr.it>
2020-10-02 14:48:35 +0200

    ettore

--- R/heatmap_signatures.R
+++ R/heatmap_signatures.R
@@ -9,7 +9,7 @@
 #' @importFrom dendextend color_branches
 #' @export
 
-heatmap_signatures <- function(X, file="heatmap_signatures.jpg", pal=NULL, n_colors=11, seurat_dendrogram=NULL) {
+heatmap_signatures <- function(X, file="heatmap_signatures.jpg", pal=NULL, n_colors=11, seurat_dendrogram=NULL, width=180, height=180, res=300, cex.axis=0.3) {
 	
 	rotate <- function(x) t(apply(x, 2, rev)) # rotate +90
 	
@@ -47,7 +47,7 @@ heatmap_signatures <- function(X, file="heatmap_signatures.jpg", pal=NULL, n_col
 	#rotation
 	X <- rotate(X)
 
-	png(file, width = 180, height = 180, res=300, units="mm")
+	jpeg(file, width = width, height = height, res=res, units="mm")
 	
 	layout.show(layout(matrix(c(1, 2, 3, 4), ncol = 2, byrow = T), heights = c(0.15, 0.95), widths = c(0.2, 0.8)))
 	
@@ -58,26 +58,26 @@ heatmap_signatures <- function(X, file="heatmap_signatures.jpg", pal=NULL, n_col
 	leg_text <- cbind(c(seq(min(X), -0.1, length.out = 5), 0, seq(0.1, max(X), length.out = 6))[-12], c(seq(min(X), -0.1, length.out = 5), 0, seq(0.1, max(X), length.out = 6))[-1])
 	leg_text <- apply(leg_text, 1, function(x) paste0(format(x[1], digits = 2), ", ", format(x[2], digits = 2)))
 	
-	legend("bottom", leg_text, pch=16, col=colors_, cex=0.3, xpd=T, pt.cex=0.6)
+	legend("bottom", leg_text, pch=16, col=colors_, cex=0.4, xpd=T, pt.cex=0.6)
 	
 	c(seq(min(X), -0.1, length.out = 5), 0, seq(0.1, max(X), length.out = 6))
 	
-	par(mar=c(0, 4, 1, 1))
+	par(mar=c(0, 2, 1, 0))
 	plot(dend_col, type = "rectangle", leaflab = "none", axes=F, edgePar = list())
 	
-	par(mar=c(3.5, 0, 0.5, 2))
+	par(mar=c(1.5, 0, 0, 2))
 	plot(rev(dend_row), type = "rectangle", leaflab = "none", axes=F, horiz = T)
 	#plot.new()
 	
-	par(mar=c(3, 5, 1, 1.5))
+	par(mar=c(3, 3, 1.5, 1))
 	image(X, xaxt="none", yaxt="none", col=colors_, breaks = c(seq(min(X), -0.1, length.out = 5), 0, seq(0.1, max(X), length.out = 6)))
 	
 	xx <- seq(0, 1, length.out = nrow(X))
 	yy <- seq(0, 1, length.out = ncol(X))
 	xx_lab <- rownames(X) #horizontal axis
 	yy_lab <- colnames(X) #vertical axis
-	axis(1, xx, xx_lab, las=2, cex.axis=0.6)
-	axis(2, yy, yy_lab, las=2, cex.axis=0.4)
+	axis(1, xx, xx_lab, las=2, cex.axis=cex.axis)
+	axis(2, yy, yy_lab, las=2, cex.axis=cex.axis)
 	
 	for(i in 1:nrow(X)){ #xx
 		for(j in 1:ncol(X)){ #yy
