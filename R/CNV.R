6abbaceee7cc865f150dbc8900a2eabf5e9413ec
Valentina <valentina.nale@itb.cnr.it>
2020-10-23 14:46:06 +0200

    valentina

--- R/CNV.R
+++ R/CNV.R
@@ -1,6 +1,6 @@
 #' CNV
 #' @param x list of dataframes retrieved by 'preprocess_object_for_cnv'.
-#' @description Function to be used in calculate_CNV.
+#' @description Function to be used in calculate_CNV. 
 #' @references "Single-cell RNA-seq highlights intratumoral heterogeneity in primary glioblastoma" Patel et al., Supplementary materials
 #' @author Valentina Nale
 
--- R/calculate_CNV.R
+++ R/calculate_CNV.R
@@ -1,11 +1,29 @@
-
 #' Calculate CNV
-#' 
+#' @param genes_by_cell Preprocessed Seurat object 
+#' @param reference_vector NULL. It is possible to add a reference vector, downloaded from GTEx portal.
 #' @export
 calculate_CNV <- function(genes_by_cells, reference_vector=NULL, mc.cores=2) {
+	# ****************************************************************
+	if (!is.null(reference_vector)) {
+		cat("Adding reference vector...\n")
+		genes_by_cells <- GetAssayData(data)
+		gbc_mean <- apply(genes_by_cells, 1, mean)
+		gtex_mean_final <-apply(GTEx_norm, 1, mean)
+		GTEx_difference <- gtex_mean_final-gbc_mean 
 	
+		new_a <- as.data.frame(genes_by_cells)
+		new_b <- as.data.frame(gtex_mean_final)
+	
+		merged_data <- merge(new_a,new_b["gtex_mean_final"],by="row.names",all.x=TRUE)
+	
+		rownames(merged_data) <- merged_data$Row.names
+		merged_data$Row.names <- NULL
+		genes_by_cells <- merged_data
+		
+	}
+	# ***************************************************************
 
-	cat("Calculating CNV...\n")	
+	cat("Preprocess object to calculate CNV...\n")	
 	ans <- preprocess_object_for_CNV(genes_by_cells)
 	
 	temp <- factor(names(ans), levels = c(1, 2, 3 ,4 ,5 ,6 ,7 ,8 , 9, 10, 11, 12 , 13, 14 ,15, 16, 17, 18, 19, 20, 21, 22, "X", "Y"))
@@ -16,24 +34,9 @@ calculate_CNV <- function(genes_by_cells, reference_vector=NULL, mc.cores=2) {
 	#exclude chrom with <100 genes
 	ans <- ans[unlist(lapply(ans, nrow)) > 100]
 	
-	# **********************************************
-	# ----> second argument for function cnv = 'reference_vector' ---> load("GTEx_ok")
-	if (is.null(reference_vector)) {
-		# do CNV on 'gene_by_cell' ---> 'ans'
-		#calculate CNV
-		cat("Calculating CNV...\n")
-		ans <- mclapply(ans, function(x) apply(x[, 4:dim(x)[2]], 2, function(y) CNV(y)), mc.cores = mc.cores)
-	} else {
-		# 'reference_vector' is present = GTEx integration
-		#w_ref <- merge(gene_by_cells, reference_vector, all.x=TRUE) --- why? 
-		cat("Adding reference...\n")
-		ans <- mclapply(ans, function(x) apply(x[, 4:dim(x)[2]], 2, function(x) x-reference_vector), mc.cores = mc.cores)
-		cat("Calculating CNV...\n")
-		ans <- mclapply(ans, function(x) apply(x[, 4:dim(x)[2]], 2, function(y) CNV(y)), mc.cores = mc.cores)
-	}
-	# **********************************************
-	
-
+	#calculate CNV
+	cat("Calculating CNV...\n")
+	ans <- mclapply(ans, function(x) apply(x[, 4:dim(x)[2]], 2, function(y) CNV(y)), mc.cores = mc.cores)
 
 	#merge chromosomes into a unique table
 	#ans <- do.call(rbind, ans)
