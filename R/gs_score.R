3ff91246e4d7f0444b33a2f083070824f47ed2b6
AliceChiodi <alice.chiodi@itb.cnr.it>
2023-09-07 16:02:31 +0200

    Update help gs_score.R

--- R/gs_score.R
+++ R/gs_score.R
@@ -11,7 +11,18 @@
 #' @param null_model boolean, whether to use the permutations or not
 #' @param na.rm whether to remove or not the missing values
 #' @importFrom Matrix colMeans colSums
-#' @return A data.frame with the following columns: case, the gene set average observed in the cell; case.N, number of genes in the gene set; case.AV, number of genes available in the cell; nmark_min, whether case.AV > nmark_min; avg_control, average of the averag gene set score in control cells; control.AV, number of available control cells, null_ok, whether a null could be defined for the cell; avg_delta_score, average of case minu each control cell; delta_score, case - avg_control.
+#' @return A data.frame with the following columns: 
+#` \itemize{
+#`  \item case: the gene set average observed in the cell; 
+#`  \item case.N: number of genes in the gene set; 
+#`  \item case.AV: number of genes available in the cell; 
+#`  \item nmark_min: whether case.AV > nmark_min; 
+#`  \item avg_control: average of the averag gene set score in control cells; 
+#`  \item control.AV: number of available control cells;
+#`  \item null_ok: whether a null could be defined for the cell; 
+#`  \item avg_delta_score: average of case minu each control cell; 
+#`  \item delta_score: case - avg_control.
+#` }
 #' @export
 #' @references Tirosh2016 10.1126/science.aad0501
 
